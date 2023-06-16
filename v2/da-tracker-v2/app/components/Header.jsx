5c042120ee8a472136fceff0aaf05d5ecaf8c515
eternalBackPain <johnnaoom@gmail.com>
2023-06-16 22:05:08 +1000

    set up routing
    playing with App directory - also failed to add a google map

---
+++ v2/da-tracker-v2/app/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <Link href='/'>Home</Link>
+            </li>
+            <li>
+                <Link href='/about'>About</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
---
+++ v2/da-tracker-v2/app/components/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//LOADING A GOOGLE MAP IS JUST TOO HARD FOR ME
+
+const Map = () => {
+  return (
+    <div>Map</div>
+  )
+}
+
+export default Map
